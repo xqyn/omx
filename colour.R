cce5bde72d8e165fa8ee93d63fec480d56d438d7
xqyn <nxquy.bms@gmail.com>
2025-04-04 12:35:10 +0200

    colour: update base_colour for vector

--- colour.R
+++ colour.R
@@ -20,39 +20,50 @@ colour.rep <- c('#F48D79', '#017075')
 
 
 # --------------------------------------------------
-
 generate_scheme <- function(base_color, 
                             n = 5, 
                             light_range = c(0.4, 0.1), 
                             dark_range = c(0.1, 0.4)) {
-    #--------------------------------------------------
-    #' Generate a Monochromatic Color Scheme
-    #'
-    #' Return a vector of lighter and darker shades based on a base color.
-    #' 
-    #' @param base_color    hex color string (e.g., "#7FC97F").
-    #' @param n             mumber of colors scheme to generate  (default is 5).
-    #' @param light_range   A numeric vector of length 2 specifying the range of lightening (default = c(0.4, 0.1)).
-    #' @param dark_range    A numeric vector of length 2 specifying the range of darkening (default = c(0.1, 0.4)).
-    #'
-    #' @return A character vector of hex color strings representing the color scheme.
-    #' @export
-    #'
-    #' @examples
-    #' generate_scheme("#7FC97F")
-    #' generate_scheme("#BEAED4", n = 7, light_range = c(0.6, 0.2), dark_range = c(0.2, 0.6))    
+        # --------------------------------------------------
+        #' Generate Monochromatic Color Schemes
+        #'
+        #' Return one or more vectors of lighter and darker shades based on one or more base colors.
+        #' If multiple base colors are supplied, a list of color schemes will be returned.
+        #'
+        #' @param base_color  A hex color string or a vector of hex strings (e.g., "#7FC97F" or c("#7FC97F", "#BEAED4")).
+        #' @param n           Number of colors in each scheme (default is 5).
+        #' @param light_range A numeric vector of length 2 specifying the range of lightening (default = c(0.4, 0.1)).
+        #' @param dark_range  A numeric vector of length 2 specifying the range of darkening (default = c(0.1, 0.4)).
+        #'
+        #' @return A character vector of hex colors if one base color is provided, or a named list of such vectors if multiple.
+        #' @export
+        #'
+        #' @examples
+        #' generate_scheme("#7FC97F")
+        #' generate_scheme(c("#7FC97F", "#BEAED4"), n = 7, light_range = c(0.6, 0.2), dark_range = c(0.2, 0.6))
+        # --------------------------------------------------
+        # internal function for a single color
+        generate_single <- function(clr) {
+        n_light <- ceiling(n / 2)
+        n_dark <- floor(n / 2)
 
-    n_light <- ceiling(n / 2)
-    n_dark <- floor(n / 2)
-    
-    lighten_vals <- seq(light_range[1], light_range[2], length.out = n_light)
-    darken_vals <- seq(dark_range[1], dark_range[2], length.out = n_dark)
-    
-    light_colors <- sapply(lighten_vals, function(a) colorspace::lighten(base_color, amount = a))
-    dark_colors <- sapply(darken_vals, function(a) colorspace::darken(base_color, amount = a))
-    
-    scheme <- c(light_colors, dark_colors)
-    return(scheme)
+        lighten_vals <- seq(light_range[1], light_range[2], length.out = n_light)
+        darken_vals  <- seq(dark_range[1], dark_range[2], length.out = n_dark)
+
+        light_colors <- sapply(lighten_vals, function(a) colorspace::lighten(clr, amount = a))
+        dark_colors  <- sapply(darken_vals,  function(a) colorspace::darken(clr, amount = a))
+
+        c(light_colors, dark_colors)
+        }
+
+        # check if base_color is a vector of length 1 or more
+        if (length(base_color) == 1) {
+        return(generate_single(base_color))
+        } else {
+        schemes <- lapply(base_color, generate_single)
+        names(schemes) <- base_color
+        return(schemes)
+        }
 }
 
 #colour <- c("#7FC97F", "#BEAED4", "#FDC086", "#FFFF99", "#C8C8C8", "#06CDE0")

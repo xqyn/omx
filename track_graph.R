f65b794ea2e005f1e868a24dc39d9cf0951a6a5d
xqyn <nxquy.bms@gmail.com>
2025-04-04 16:24:56 +0200

    track: update size and alpha for point

--- track_graph.R
+++ track_graph.R
@@ -14,6 +14,8 @@ library(ggplot2)
 ## Wide Interval: More uncertainty, broader shading (e.g., sparse data, high variability, higher level).
 ## Narrow Interval: Less uncertainty, tighter shading (e.g., dense data, low variability, lower level).
 
+
+# --------------------------------------------------
 plot_bigwig <- function(data, 
                         figure_dir, 
                         file_name, 
@@ -21,70 +23,71 @@ plot_bigwig <- function(data,
                         title = "chr:start-end",
                         feature1 = "day", 
                         feature2 = "week", 
-                        do_smooth = TRUE) {
+                        smooth = TRUE, 
+                        vline_values = NULL,    # New parameter for vertical lines
+                        span = 0.1,             # Default span
+                        level = 0.95,           # Default confidence level
+                        se = FALSE) {           # Default for confidence interval display
   
   # Check if data is provided
   if (is.null(data)) stop("Providing data is required.")
+  if (!all(c(feature1, feature2) %in% names(data))) {
+  stop("Both 'feature1' and 'feature2' must be present in the data.")
+  }
   
   # Message: Starting the plot creation process
   message("Starting the plot creation process.")
   
   # Start the ggplot object
-  p <- ggplot()
+  p <- ggplot(data, aes(x = start, y = score))
   
+  p <- p + geom_point(aes(color = .data[[feature1]]), 
+                      size = 1,
+                      alpha = 0.15)
+
   # Add geom based on smoothing choice
-  if (do_smooth) {
+if (smooth) {
     message("Adding smoothed lines with confidence intervals.")
-    p <- p + geom_smooth(data = data, 
-                         aes(x = start, y = score, 
-                             color = .data[[feature1]], 
-                             fill = .data[[feature1]]),  # Dynamic color/fill
-                         method = "loess", span = 0.1, 
-                         size = 1.5,  # Thicker main line
-                         level = 0.95,  # 95% confidence interval
-                         alpha = 0.75,  # Light shading for visibility
-                         se = FALSE)  # Include confidence interval
-  } else {
-    message("Adding points with smoothed lines (without confidence intervals).")
-    p <- p + geom_point(data = data, 
-                        aes(x = start, y = score, 
-                            color = .data[[feature1]]),  # Dynamic color for points
-                        size = 2,
-                        alpha = 0.15) + # Point size
-            geom_smooth(data = data, 
-                         aes(x = start, y = score, 
-                             color = .data[[feature1]], 
-                             fill = .data[[feature1]]),  # Dynamic color/fill
-                         method = "loess", span = 0.1, 
-                         size = 1.5,  # Thicker main line
-                         level = 0.95,  # 95% confidence interval
-                         alpha = 0.75,  # Light shading for visibility
-                         se = FALSE)  # Include confidence interval
+    p <- p + geom_smooth(aes(color = .data[[feature1]], 
+                            fill = .data[[feature1]]), 
+                         method = "loess", span = span, 
+                         size = 1.5, 
+                         level = level, 
+                         alpha = 0.75, 
+                         se = se)
   }
-  
-  # Message: Adding labels, scales, and faceting
+# Message: Adding labels, scales, and faceting
   message("Adding labels, color scales, and facet wrapping.")
   
   # Add labels, scales, faceting, and theme
   p <- p +
     labs(x = "Genomic Position", y = "Coverage", 
          title = title) +
-    scale_color_manual(values = setNames(data[[colour_set]], data[[feature1]])) +  # Dynamic color mapping
-    scale_fill_manual(values = setNames(data[[colour_set]], data[[feature1]])) +   # Dynamic fill mapping
-    coord_cartesian(ylim = c(0, max(data$score))) +  # Adjust y-axis
-    facet_wrap(as.formula(paste("~", feature2)), ncol = 1) +  # Dynamic faceting
+    scale_color_manual(values = setNames(data[[colour_set]], data[[feature1]])) +
+    scale_fill_manual(values = setNames(data[[colour_set]], data[[feature1]])) +
+    coord_cartesian(ylim = c(0, ceiling(max(data$score, na.rm = TRUE) / 500) * 500)) +
+    facet_wrap(as.formula(paste("~", feature2)), ncol = 1) +
+    geom_vline(xintercept = vline_values, linetype = "dashed", color = "red") +
     theme_minimal()
-  
+  # Customize legend based on se
+  # Customize legend based on smooth and se, with alpha = 1
+  if (!smooth) {
+      p <- p + guides(fill = "none", 
+                      color = guide_legend(override.aes = list(linetype = 1, 
+                      size = 3, 
+                      alpha = 1)))
+    }
+
   # Message: Saving the plot
   message("Saving the plot to the specified directory.")
   
   # Save the plot
   ggsave(paste0(figure_dir, file_name), 
-         plot = p, bg = 'white', height = 10, width = 20, dpi = 320)
+         plot = p, bg = 'white', height = 10, width = 16.2, dpi = 320)
   
   # Message: Plotting completed
   message("Plotting completed. Plot saved.")
   
-  # Return the plot object for previewing if desired
+  # Return the plot object
   return(p)
 }
